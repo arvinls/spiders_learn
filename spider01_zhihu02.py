2f326d58ffb0c9d36fbff360ce186caa25bfb2b9
arvinls <arvinls@users.noreply.github.com>
2015-10-19 16:31:52 +0800

    get the vote of an answer in a question

--- spider01_zhihu02.py
+++ spider01_zhihu02.py
@@ -4,3 +4,21 @@ __author__ = 'arvin'
 
 #this program is get the answer of a question
 
+import requests
+import re
+
+
+lst = []
+
+url = 'http://www.zhihu.com/question/33946642'
+r = requests.get(url)
+
+html = r.text
+
+vote_count_search = re.compile(r'<span class="count">(\d*?)</span>')
+
+vote_count_num = vote_count_search.findall(html)
+
+#get the  vote_num
+for i in vote_count_num:
+	print i
