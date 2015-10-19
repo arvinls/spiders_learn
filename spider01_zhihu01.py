5e17debcabb69f66433cdc9b47317be480b868c7
arvinls <arvinls@users.noreply.github.com>
2015-10-19 15:49:56 +0800

    Update spider01_zhihu01.py

--- spider01_zhihu01.py
+++ spider01_zhihu01.py
@@ -1,18 +1,34 @@
+# coding=utf-8
+# __author__ = 'arvin'
+
 import requests
 import re
 
 
-for i in xrange(1, 7):
+lst = []
+
+for i in xrange(1, 50):
 
 	url = 'http://www.zhihu.com/topic/19577498?page=%s' % str(i)
 	r = requests.get(url)
+
+	if r.status_code != 200:
+		print 'THIS IS THE END!'
+		break
 
 	select = re.compile(r'<a class="question_link" target="_blank" href="(.*?)">(.*?)</a>')
 
 	html = r.text
 
-	lst = select.findall(html)
 
-	for i in lst:
-		print 'address:  ', 'http://www.zhihu.com' + i[0]
-		print 'question: ', i[1]
+	results = select.findall(html)
+	
+	lst.extend(results)
+
+
+for i in lst:
+	address_lst = 'http://www.zhihu.com' + i[0]
+	question_lst = i[1]
+
+	print address.encode('utf-8')
+	print question.encode('utf-8')
